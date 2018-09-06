eae056ea705897bf64fc3b73c649243747ab600b
Ruth Brand <ruthbrandsoprano@gmail.com>
2018-09-06 11:45:49 -0600

    didn't finish through iteration three, but wrote down ALL the questions I have that I believe if i had the answers to i could solve it

--- lib/dock.rb
+++ lib/dock.rb
@@ -18,15 +18,20 @@ class Dock
 
   #  def log_hour
   # #  #ok so i want this to look at any boats that are not returned and add an hour to them
-  # #  #also it wants to log the hours of any boats that were returned as well
-  # #  #how the crap do i get the boats in here without using an argument?????
+       #like, if boat.return == false, add an hour to the hours_rented
+  # #  #how the crap do i get the boats in here without using an argument though?
+      # i cant use the return value of rent or return because those both require arguments but i'd need to pass them
+      # in from somewhere. 
   #  end
+  #
+  # def revenue
 
-  def revenue
-    #seriously why can i formulate an explination but not write it in code? i'm so frustrated
-    #this method will take how ever many hours total the boat was rented for up to the
-    #max and charge it by the boats price per hour FOR EACH BOAT and then add them together
-  end
+  #   #this method will take how ever many hours total the boat was rented for up to the
+  #   #max amount allowed by the dock and charge it by the boats price per hour FOR EACH BOAT and then add them together
+  # end
+
+  # => ok what pieces of knowledge am i missing in order to DO this? I don't even know what I'm trying to ask
+  # i know what each method would need to do but there's little details in how to do it that I am missing
 
 
 end

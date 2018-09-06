09665d749f05d6dfcfd1229fdcc68a957bc0f1f4
Ruth Brand <ruthbrandsoprano@gmail.com>
2018-09-06 11:08:10 -0600

    argh

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,8 +1,32 @@
 class Dock
+  attr_reader :name,
+              :max_rental_time
 
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
   end
-  
+
+  def rent(boat, renter)
+    #ok this method begins tracking how long the boat is rented for
+    boat.add_hour
+    boat.hours_rented
+  end
+
+   def log_hour
+  #  #ok so i want this to look at any boats that are not returned and add an hour to them
+  #  #also it wants to log the hours of any boats that were returned as well
+  #  #how the crap do i get the boats in here without using an argument?????
+   end
+
+  def return
+    #this method will stop the dock from tracking how long the boat is rented for
+
+  end
+
+  def total
+    #this method will take how ever many hours total the boat was rented for up to the
+    #max and charge it by the boats price per hour
+  end
+
 end
--- test/dock_test.rb
+++ test/dock_test.rb
@@ -1,6 +1,8 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/dock'
+require './lib/renter'
+require './lib/boat'
 require 'pry'
 
 class DockTest<Minitest::Test
@@ -19,5 +21,31 @@ class DockTest<Minitest::Test
    dock = Dock.new("The Rowing Dock", 3)
    assert_equal 3, dock.max_rental_time
   end
-  
+
+  def test_i_can_track_how_long_the_boats_have_been_rented_out_when_first_rented
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    canoe = Boat.new(:canoe, 30)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    sup_2 = Boat.new(:standup_paddle_board, 15)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")
+    assert_equal 1, dock.rent(kayak_1, patrick)
+  end
+
+  def test_the_kayaks_are_logged_for_two_hours_each_when_i_call_log_hour_the_first_time
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    # canoe = Boat.new(:canoe, 30)
+    # sup_1 = Boat.new(:standup_paddle_board, 15)
+    # sup_2 = Boat.new(:standup_paddle_board, 15)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+    assert_equal 4, dock.log_hour
+  end
+
+
 end

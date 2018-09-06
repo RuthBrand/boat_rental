fa91bc1777e37f1bcd127d31c04fe0121f31282f
Ruth Brand <ruthbrandsoprano@gmail.com>
2018-09-06 11:41:20 -0600

    oof

--- lib/dock.rb
+++ lib/dock.rb
@@ -8,25 +8,25 @@ class Dock
   end
 
   def rent(boat, renter)
-    #ok this method begins tracking how long the boat is rented for
     boat.add_hour
     boat.hours_rented
   end
 
-   def log_hour
-  #  #ok so i want this to look at any boats that are not returned and add an hour to them
-  #  #also it wants to log the hours of any boats that were returned as well
-  #  #how the crap do i get the boats in here without using an argument?????
-   end
-
-  def return
-    #this method will stop the dock from tracking how long the boat is rented for
-
+  def return(boat)
+    @retuned = boat.hours_rented
   end
 
-  def total
+  #  def log_hour
+  # #  #ok so i want this to look at any boats that are not returned and add an hour to them
+  # #  #also it wants to log the hours of any boats that were returned as well
+  # #  #how the crap do i get the boats in here without using an argument?????
+  #  end
+
+  def revenue
+    #seriously why can i formulate an explination but not write it in code? i'm so frustrated
     #this method will take how ever many hours total the boat was rented for up to the
-    #max and charge it by the boats price per hour
+    #max and charge it by the boats price per hour FOR EACH BOAT and then add them together
   end
+
 
 end
--- test/dock_test.rb
+++ test/dock_test.rb
@@ -34,18 +34,30 @@ class DockTest<Minitest::Test
     assert_equal 1, dock.rent(kayak_1, patrick)
   end
 
-  def test_the_kayaks_are_logged_for_two_hours_each_when_i_call_log_hour_the_first_time
-    dock = Dock.new("The Rowing Dock", 3)
-    kayak_1 = Boat.new(:kayak, 20)
-    kayak_2 = Boat.new(:kayak, 20)
-    # canoe = Boat.new(:canoe, 30)
-    # sup_1 = Boat.new(:standup_paddle_board, 15)
-    # sup_2 = Boat.new(:standup_paddle_board, 15)
-    patrick = Renter.new("Patrick Star", "4242424242424242")
-    dock.rent(kayak_1, patrick)
-    dock.rent(kayak_2, patrick)
-    assert_equal 4, dock.log_hour
-  end
+  # def test_the_kayaks_are_logged_for_two_hours_each_when_i_call_log_hour_the_first_time
+  #   dock = Dock.new("The Rowing Dock", 3)
+  #   kayak_1 = Boat.new(:kayak, 20)
+  #   kayak_2 = Boat.new(:kayak, 20)
+  #   # canoe = Boat.new(:canoe, 30)
+  #   # sup_1 = Boat.new(:standup_paddle_board, 15)
+  #   # sup_2 = Boat.new(:standup_paddle_board, 15)
+  #   patrick = Renter.new("Patrick Star", "4242424242424242")
+  #   dock.rent(kayak_1, patrick)
+  #   dock.rent(kayak_2, patrick)
+  #   binding.pry
+  #   assert_equal 4, dock.log_hour
+  # end
 
+ def test_the_return_method_tells_me_how_many_hours_the_boat_was_rented_for
+     dock = Dock.new("The Rowing Dock", 3)
+     kayak_1 = Boat.new(:kayak, 20)
+     kayak_2 = Boat.new(:kayak, 20)
+     # canoe = Boat.new(:canoe, 30)
+     # sup_1 = Boat.new(:standup_paddle_board, 15)
+     # sup_2 = Boat.new(:standup_paddle_board, 15)
+     patrick = Renter.new("Patrick Star", "4242424242424242")
+     dock.rent(kayak_1, patrick)
+     assert_equal 1, dock.return(kayak_1)
+  end
 
 end
